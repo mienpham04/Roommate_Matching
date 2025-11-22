66c65e79866bd755e95f2afa43517eedda62adb1
mienpham04 <phammocmien04@gmail.com>
2025-11-22 00:02:29 -0500

    frontend setup

--- backend/src/main/java/com/roommate/hackathon/HelloController.java
+++ backend/src/main/java/com/roommate/hackathon/HelloController.java
@@ -1,17 +1,17 @@
 package com.roommate.hackathon;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String requestMethodName(@RequestParam String param) {
-        return new String();
-    }
     
+    // public String requestMethodName(@RequestParam String param) {
+    //     return new String();
+    // }
+    
+    @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
     }
