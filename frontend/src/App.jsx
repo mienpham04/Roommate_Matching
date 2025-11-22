0a6402a7cad526f94d9838e8401c1e1b68659338
mienpham04 <phammocmien04@gmail.com>
2025-11-22 03:45:48 -0500

    done setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import { Routes, Route } from "react-router";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 
 function App() {
+  
   return (
+    <>
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route path="/profile/:id" element={<ProfilePage />}/>
 
     </Routes>
+
+    <Toaster toastOptions={{ duration: 3000 }}/>
+    </>
+    
   );
 }
 
 export default App;
+
+// tailwind, daisyui, react-router, react-hot-toast
+// react-query aka tanstack query, axios
+//        (to fetch query)
+// https://tanstack.com/query/latest
