3ac6f2bf9eef007d5af2aafcce21395aea24b196
Long Huynh <longmien@Mac.attlocal.net>
2025-11-22 03:48:08 -0600

    setup db

---
+++ backend/src/main/java/com/roommate/manager/controller/HelloController.java
@@ -0,0 +1,34 @@
+package com.roommate.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.roommate.manager.model.TestModel;
+import com.roommate.manager.repository.TestRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    private final TestRepository testRepository;
+
+    public HelloController(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
+    
+
+    @GetMapping("/create")
+    public String create() {
+        TestModel testModel = new TestModel();
+        testModel.setName("Sample Name");
+        testRepository.save(testModel);
+        return "Created TestModel with ID: " + testModel.getId();
+    }
+    
+    public String hello() {
+        return "Hello World";
+    }
+}
