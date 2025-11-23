a7c2fcbbab7510286bb599af700e6c28720a5d67
mienpham04 <phammocmien04@gmail.com>
2025-11-22 19:14:23 -0500

    remove .env file

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function ProfilePage() {
   return (
-    <div>
-      Profile Page
-    </div>
+    <>
+    <Navbar />
+    </>
   )
 }
 
