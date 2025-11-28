72d435c095f88db30a412c98a5cc101f74f169c3
Long Huynh <longtu19@users.noreply.github.com>
2025-11-27 23:24:53 -0600

    vector search implementation

--- backend/src/main/java/com/roommate/manager/controller/UserController.java
+++ backend/src/main/java/com/roommate/manager/controller/UserController.java
@@ -2,6 +2,7 @@ package com.roommate.manager.controller;
 
 import com.roommate.manager.model.UserModel;
 import com.roommate.manager.repository.UserRepository;
+import com.roommate.manager.service.IndexManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,10 +16,24 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private IndexManagementService indexManagementService;
+
     // CREATE
     @PostMapping("/new_user")
     public UserModel createUser(@RequestBody UserModel user) {
-        return userRepository.save(user);
+        // Save user to MongoDB
+        UserModel savedUser = userRepository.save(user);
+
+        // Upload vector to index (async - don't block on failures)
+        try {
+            indexManagementService.uploadUserToIndex(savedUser);
+        } catch (Exception e) {
+            System.err.println("Warning: Failed to upload user vector to index: " + e.getMessage());
+            // Continue anyway - user is saved in MongoDB
+        }
+
+        return savedUser;
     }
 
     // READ (Get all users)
@@ -37,12 +52,28 @@ public class UserController {
     @PutMapping("/{id}")
     public UserModel updateUser(@PathVariable String id, @RequestBody UserModel updatedUser) {
         updatedUser.setId(id);
-        return userRepository.save(updatedUser);
+        UserModel savedUser = userRepository.save(updatedUser);
+
+        // Update vector in index
+        try {
+            indexManagementService.uploadUserToIndex(savedUser);
+        } catch (Exception e) {
+            System.err.println("Warning: Failed to update user vector in index: " + e.getMessage());
+        }
+
+        return savedUser;
     }
 
     // DELETE
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable String id) {
         userRepository.deleteById(id);
+
+        // Remove vector from index
+        try {
+            indexManagementService.removeUserFromIndex(id);
+        } catch (Exception e) {
+            System.err.println("Warning: Failed to remove user vector from index: " + e.getMessage());
+        }
     }
 }
